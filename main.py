9b6e7dd6326774e62cb2df8ab046699d96794ca0
Aarshs <54901482+Aarshs@users.noreply.github.com>
2020-10-10 08:25:20 -0600

    added main file anda couple functions

---
+++ main.py
@@ -0,0 +1,30 @@
+# print("Please input the date in numerical form")
+# day = int(input("Please input the day\n"))
+# month = int(input("\nPlease input the month\n"))
+# year = int(input("\nPlease input the year\n"))
+
+year = 1978
+
+def leap_year():
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                if month == 1 or 2:
+                    # Call leap year function
+                else:
+                    # Call non-leap year function
+            else:
+                # Call non-leap year function
+        elif:
+            # Call non-leap year function
+    else:
+        # Call non-leap year function
+
+
+def doomsday_century():
+    year = str(year)
+    year[0]
+    print(year)
+
+    century = year // 100
+doomsday_century()
---
+++ psuedocode.py
@@ -0,0 +1,18 @@
+# July 20, 2020
+
+# First figure out if the year is leap year
+
+# What doomsday is it by century 2000 - Tuesday - 2
+
+# 1st finger - 2
+# 2nd finger - 20/12 = 1 R 8
+# 3rd finger - 8
+# 4th finegr - 8/4 = 2
+# == 13
+# Which is 13/7 = 1 R 6
+# Since r = 6 doomsday is sixturday
+
+# Since doomsday for July is 7/11, 7/11 is saturday
+# 20 - 11 = 9, so 9 days after saturday
+# 9/7 = 1 R 2, since R = 2, 2 days after saturday
+#  == Monday
